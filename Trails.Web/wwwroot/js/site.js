a96b3d23650b915dd462aa3441966f81a16e7da9
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-01-28 12:08:42 +0200

    move js script for key generation in separate file so it load only when view is called not the whole layout

--- Trails.Web/wwwroot/js/site.js
+++ Trails.Web/wwwroot/js/site.js
@@ -1,23 +1,6 @@
 ﻿window.addEventListener('DOMContentLoaded', event => {
 
-    //create beacon generate key call to endpoint to retrieve rnd key
-    document.getElementById('gen-btn').addEventListener('click', async () => {
-        let keyField = document.getElementById('key-field');
-        let infoLabel = document.getElementById('info-label');
-        let response = await fetch('/Administration/Beacon/getkey');
-        if (response.ok) {
-            let result = await response.json();
-            keyField.value = result.key;
-        } else {
-            infoLabel.textContent = 'Error getting key from server! Type strong key by yourself or try again later...';
-        }
-    });
-
-    //create beacon copy key 
-    document.getElementById('copy-key').addEventListener('click', () => {
-        navigator.clipboard.writeText(document.getElementById("key-field").value);
-        document.getElementById('info-label').textContent = 'Copied!';
-    });
+    
 
     // Navbar shrink function
     var navbarShrink = function () {
