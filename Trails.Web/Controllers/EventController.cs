4e00c6c48a7342aaed5797672ec50b87fd806749
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-02-12 22:41:14 +0200

    implemented additional action methods for event

--- Trails.Web/Controllers/EventController.cs
+++ Trails.Web/Controllers/EventController.cs
@@ -54,6 +54,12 @@ namespace Trails.Web.Controllers
                 return View(eventFormModel);
             }
 
+            if (DateTime.UtcNow > eventFormModel.StartDate.AddDays(-3))
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = ErrorMessages.EventThreeDaysBeforeStartError;
+                return View(eventFormModel);
+            }
+
             var resultId = await this.eventService
                 .CreateEventAsync(eventFormModel, currentUserId, imgFile);
 
@@ -78,6 +84,80 @@ namespace Trails.Web.Controllers
             }
 
             return View(@event);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var changeState = await this.eventService
+                .DeleteEventAsync(id);
+
+            if (!changeState)
+            {
+                return View("Error");
+            }
+
+            TempData[NotificationConstants.TempDataKeySuccess] = NotificationConstants.EventDeleteSuccess;
+            return RedirectToAction("All");
+        }
+
+        public async Task<IActionResult> Apply(string userId, string eventId)
+        {
+            var hasApplied = await this.eventService
+                .ApplyForEventAsync(userId, eventId);
+
+            if (!hasApplied)
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = NotificationConstants.ParticipantAlreadyAppliedError;
+            }
+
+            TempData[NotificationConstants.TempDataKeySuccess] = NotificationConstants.ParticipantApplicationSuccess;
+            return RedirectToAction("Details", new { eventId });
+        }
+
+        public async Task<IActionResult> ApproveParticipant(string participantId,string eventId)
+        {
+            var participantState = await this.eventService
+                .ApproveParticipantAsync(participantId,eventId);
+
+            if (!participantState)
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = NotificationConstants.ParticipantApproveError;
+                return RedirectToAction("Details", new {eventId});
+            }
+
+            return NoContent();
+        }
+
+        public async Task<IActionResult> EditImage(string eventId)
+        {
+            var imgFile = Request.Form.Files.First();
+
+            if (imgFile == null)
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = NotificationConstants.MissingEventImageError;
+                return RedirectToAction("Details", new { eventId });
+            }
+
+            if (!ValidateImageExtension(imgFile))
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = ErrorMessages.ImageFileExtensionError;
+                return RedirectToAction("Details", new { eventId });
+            }
+
+            var currentUserId = this.userManager
+                .GetUserId(this.User);
+
+            var edited = await this.eventService
+                .EditImageAsync(currentUserId, eventId, imgFile);
+
+            if (!edited)
+            {
+                TempData[NotificationConstants.TempDataKeyFail] = NotificationConstants.EventImageEditError;
+                return RedirectToAction("Details", new { eventId });
+            }
+
+            TempData[NotificationConstants.TempDataKeySuccess] = NotificationConstants.EventImageEditSuccess;
+            return RedirectToAction("Details", new{eventId});
         }
 
         private bool ValidateImageExtension(IFormFile imgFile)
