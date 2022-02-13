c028f835c4bb647423a8320e2d3c32afcf2ad5e6
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-02-14 00:07:43 +0200

    add route details model

---
+++ Trails.Web/Models/Route/RouteDetailsModel.cs
@@ -0,0 +1,25 @@
+﻿using Trails.Web.Models.RoutePoint;
+
+namespace Trails.Web.Models.Route
+{
+    public class RouteDetailsModel : IRouteModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StartLocationName { get; set; }
+
+        public string FinishLocationName { get; set; }
+
+        public double Length { get; set; }
+
+        public double MinimumAltitude { get; set; }
+
+        public double MaximumAltitude { get; set; }
+
+        public string CreatorId { get; set; }
+
+        public List<RoutePointExportModel> RoutePoints { get; set; }
+    }
+}
