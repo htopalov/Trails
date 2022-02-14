3c7cc508a58df626d72f5c72df844d5364b473fe
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-02-14 21:05:10 +0200

    add models

---
+++ Trails.Web/Models/Route/IRouteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Trails.Web.Models.Route
+{
+    public interface IRouteModel
+    {
+        public string Name { get; set; }
+
+        public string StartLocationName { get; set; }
+
+        public string FinishLocationName { get; set; }
+    }
+}
