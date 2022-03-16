66c8d93a4dd2095d0d4cb2b7b270dd7b6641b6e9
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-03-16 21:01:16 +0200

    removed reverse mapping where not needed

--- Trails.Infrastructure/MappingProfile.cs
+++ Trails.Infrastructure/MappingProfile.cs
@@ -19,93 +19,74 @@ namespace Trails.Infrastructure
                 .ForMember(dest => dest.KeyHash, opt => opt.MapFrom(s => s.Key))
                 .ReverseMap();
 
-            this.CreateMap<BaseBeaconModel, Beacon>()
-                .ReverseMap();
+            this.CreateMap<Beacon, BaseBeaconModel>();
 
             this.CreateMap<EventFormModel, Event>()
-                .ForMember(dest => dest.Image, opt => opt.Ignore())
-                .ReverseMap();
+                .ForMember(dest => dest.Image, opt => opt.Ignore());
 
             this.CreateMap<RouteCreateModel, Route>()
-                .ForMember(dest => dest.RoutePoints, opt => opt.Ignore())
-                .ReverseMap();
+                .ForMember(dest => dest.RoutePoints, opt => opt.Ignore());
 
             this.CreateMap<Event, EventDetailsModel>()
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(s => ImageProcessor.ProcessImageFromDb(s)))
-                .ForMember(dest=>dest.CreatorFullName, opt => opt.MapFrom(s => $"{s.Creator.FirstName} {s.Creator.LastName}"))
-                .ReverseMap();
+                .ForMember(dest => dest.CreatorFullName,
+                    opt => opt.MapFrom(s => $"{s.Creator.FirstName} {s.Creator.LastName}"));
 
-            this.CreateMap<Event, EventEditFormModel>()
-                .ReverseMap();
+            this.CreateMap<Event, EventEditFormModel>();
 
             this.CreateMap<Route, RouteDetailsModel>()
-                .ForMember(dest=>dest.RoutePoints, opt=>opt.MapFrom(s=>s.RoutePoints.OrderBy(p=>p.OrderNumber)))
-                .ForMember(dest=>dest.HasEventStarted, opt=>opt.MapFrom(s=>s.Event.StartDate < DateTime.UtcNow))
-                .ReverseMap();
+                .ForMember(dest => dest.RoutePoints, opt => opt.MapFrom(s => s.RoutePoints.OrderBy(p => p.OrderNumber)))
+                .ForMember(dest => dest.HasEventStarted, opt => opt.MapFrom(s => s.Event.StartDate < DateTime.UtcNow));
 
             this.CreateMap<Route, RouteEditFormModel>()
                 .ReverseMap();
 
-            this.CreateMap<BaseRouteModel, Route>()
-                .ReverseMap();
+            this.CreateMap<Route, BaseRouteModel>();
 
-            this.CreateMap<RoutePoint, RoutePointExportModel>()
-                .ReverseMap();
+            this.CreateMap<RoutePoint, RoutePointExportModel>();
 
-            this.CreateMap<RoutePoint, ExportPointModel>()
-                .ReverseMap();
+            this.CreateMap<RoutePoint, ExportPointModel>();
 
             this.CreateMap<Event, FirstToStartEventCardModel>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(s=> ImageProcessor.ProcessImageFromDb(s)))
-                .ReverseMap();
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(s => ImageProcessor.ProcessImageFromDb(s)));
 
-            this.CreateMap<BaseEventModel, Event>()
-                .ReverseMap();
+            this.CreateMap<Event, BaseEventModel> ();
 
             this.CreateMap<Event, UnapprovedEventDetailsModel>()
-                .ForMember(dest=>dest.Creator, opt=>opt.MapFrom(s=> $"{s.Creator.FirstName} {s.Creator.LastName}"))
-                .ForMember(dest=>dest.CreatorPhoneNumber, opt=>opt.MapFrom(s=>s.Creator.PhoneNumber))
-                .ForMember(dest=>dest.CreatorEmail, opt=>opt.MapFrom(s=>s.Creator.Email))
+                .ForMember(dest => dest.Creator, opt => opt.MapFrom(s => $"{s.Creator.FirstName} {s.Creator.LastName}"))
+                .ForMember(dest => dest.CreatorPhoneNumber, opt => opt.MapFrom(s => s.Creator.PhoneNumber))
+                .ForMember(dest => dest.CreatorEmail, opt => opt.MapFrom(s => s.Creator.Email))
                 .ReverseMap();
 
             this.CreateMap<Event, UnapprovedEventModel>()
-                .ForMember(dest=>dest.DetailsModel,opt=>opt.MapFrom(s=>s))
-                .ReverseMap();
+                .ForMember(dest=>dest.DetailsModel,opt=>opt.MapFrom(s=>s));
 
-            this.CreateMap<Event, EventPreparationModel>()
-                .ReverseMap();
+            this.CreateMap<Event, EventPreparationModel>();
 
-            this.CreateMap<Participant, ParticipantPreparationModel>()
-                .ForMember(dest=>dest.FullName, opt=>opt.MapFrom(s=> $"{s.User.FirstName} {s.User.LastName}"))
-                .ReverseMap();
+                this.CreateMap<Participant, ParticipantPreparationModel>()
+                .ForMember(dest=>dest.FullName, opt=>opt.MapFrom(s=> $"{s.User.FirstName} {s.User.LastName}"));
 
-            this.CreateMap<Beacon, BeaconPreparationModel>()
-                .ReverseMap();
+            this.CreateMap<Beacon, BeaconPreparationModel>();
 
-            this.CreateMap<BeaconData, ExportPointModel>()
-                .ReverseMap();
+            this.CreateMap<BeaconData, ExportPointModel>();
 
-            this.CreateMap<BeaconDataDtoPost, BeaconData>()
-                .ReverseMap();
+            this.CreateMap<BeaconDataDtoPost, BeaconData>();
 
             this.CreateMap<Event, LiveEventCardModel>()
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(s => ImageProcessor.ProcessImageFromDb(s)))
-                .ReverseMap();
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(s => ImageProcessor.ProcessImageFromDb(s)));
 
-            this.CreateMap<BeaconData, BeaconDataBroadcastModel>()
-                .ReverseMap();
+            this.CreateMap<BeaconData, BeaconDataBroadcastModel>();
 
             this.CreateMap<Participant, LiveParticipantDetailsModel>()
                 .ForMember(dest => dest.Fullname, opt => opt.MapFrom(s => $"{s.User.FirstName} {s.User.LastName}"))
-                .ForMember(dest=>dest.CountryName, opt=>opt.MapFrom(s=>s.User.CountryName))
-                .ForMember(dest=>dest.Gender, opt=>opt.MapFrom(s=>s.User.Gender))
-                .ForMember(dest=>dest.BeaconData, opt=>opt.MapFrom(s=>s.BeaconData.OrderBy(bd=>bd.Timestamp)))
-                .ReverseMap();
+                .ForMember(dest => dest.CountryName, opt => opt.MapFrom(s => s.User.CountryName))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(s => s.User.Gender))
+                .ForMember(dest => dest.BeaconData, opt => opt.MapFrom(s => s.BeaconData.OrderBy(bd => bd.Timestamp)));
 
             this.CreateMap<Event, LiveEventDetailsModel>()
-                .ForMember(dest=>dest.RoutePoints, opt=>opt.MapFrom(s=>s.Route.RoutePoints.OrderBy(r=>r.OrderNumber)))
-                .ForMember(dest=>dest.Participants, opt=>opt.MapFrom(s=>s.Participants))
-                .ReverseMap();
+                .ForMember(dest => dest.RoutePoints,
+                    opt => opt.MapFrom(s => s.Route.RoutePoints.OrderBy(r => r.OrderNumber)))
+                .ForMember(dest => dest.Participants, opt => opt.MapFrom(s => s.Participants));
         }
     }
 }
