252d9f1b228683e02c1d30011fa1173f8a174939
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-04-06 22:57:16 +0300

    home controller tests

---
+++ Trails.Test.Web/HomeController/HomeControllerTest.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Trails.Test.Web.Helpers;
+using Xunit;
+using static Trails.Test.Web.HomeController.HomeControllerTestConstants;
+using HttpMethod = System.Net.Http.HttpMethod;
+
+namespace Trails.Test.Web.HomeController
+{
+    public class HomeControllerTest
+    {
+        private readonly WebAppFactoryWithoutAuth<Program> anonymousServer;
+        private readonly WebAppFactoryWithAuth<Program> authenticatedServer;
+
+        public HomeControllerTest()
+        {
+            this.anonymousServer = new WebAppFactoryWithoutAuth<Program>();
+            this.authenticatedServer = new WebAppFactoryWithAuth<Program>();
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task HomeControllerShouldReturnAnonymousPartialViewWhenAnonymousUser(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedHomeControllerParagraphFromAnonymousPartial, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task HomeControllerShouldReturnLoggedInPartialViewWhenLoggedUser(string url)
+        {
+            var client = authenticatedServer.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedHeaderWhenLogged, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/identity/account/login")]
+        public async Task HomeControllerShouldReloadLoginViewWhenInputIncorrect(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var initialResponse = await client.GetAsync(url);
+            var antiForgeryValues = await AfTokenExtractor.ExtractAntiForgeryValues(initialResponse);
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, url);
+            var formModel = new Dictionary<string, string>
+            {
+                {AfTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue},
+                { "Email", "test@test.com"},
+                {"Password", "123456"}
+            };
+            postRequest.Content = new FormUrlEncodedContent(formModel);
+            var response = await client.SendAsync(postRequest);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedErrorMessageInvalidLogin, responseContent);
+        }
+
+
+        [Theory]
+        [InlineData("/home/faq")]
+        public async Task HomeControllerShouldReturnFaqPage(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedHeadingInFaqView, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/home/error")]
+        public async Task HomeControllerShouldReturnErrorPage(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedHeadingInErrorView, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/home/contact")]
+        public async Task HomeControllerShouldReturnContactPage(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedHeadingInContactPage, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/home/contact")]
+        public async Task ContactPagePostRequestInFormShouldRedirectToIndexAfterCompletion(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var initialResponse = await client.GetAsync(url);
+            var antiForgeryValues = await AfTokenExtractor.ExtractAntiForgeryValues(initialResponse);
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, url);
+            var formModel = new Dictionary<string, string>
+            {
+                {AfTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue},
+                {"Fullname", "Borislav Mihaylov"},
+                { "Email", "bmihaylov@test.com"},
+                {"PhoneNumber", "0885678901"},
+                {"Message", "Test message from anonymous user."}
+            };
+            postRequest.Content = new FormUrlEncodedContent(formModel);
+            var response = await client.SendAsync(postRequest);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedMessageNotificationAfterContact, responseContent);
+        }
+
+        [Theory]
+        [InlineData("/home/contact")]
+        public async Task ContactPagePostRequestInFormShouldReloadIfValidationsDoNotPass(string url)
+        {
+            var client = anonymousServer.CreateClient();
+            var initialResponse = await client.GetAsync(url);
+            var antiForgeryValues = await AfTokenExtractor.ExtractAntiForgeryValues(initialResponse);
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, url);
+            var formModel = new Dictionary<string, string>
+            {
+                {AfTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue},
+                {"Fullname", ""},
+                { "Email", "bmihaylov@test.com"},
+                {"PhoneNumber", "0885678"},
+                {"Message", "Test"}
+            };
+            postRequest.Content = new FormUrlEncodedContent(formModel);
+            var response = await client.SendAsync(postRequest);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedErrorForMessage, responseContent);
+            Assert.Contains(ExpectedErrorForFullname, responseContent);
+            Assert.Contains(ExpectedErrorForPhoneFormat, responseContent);
+        }
+    }
+}
---
+++ Trails.Test.Web/HomeController/HomeControllerTestConstants.cs
@@ -0,0 +1,32 @@
+﻿namespace Trails.Test.Web.HomeController
+{
+    public static class HomeControllerTestConstants
+    {
+        public const string ExpectedHomeControllerParagraphFromAnonymousPartial =
+            "<p class=\"masthead-subheading font-weight-light mb-0\">Trail running - Offroad Cycling - Orienteering - Speed Hiking - More...</p>";
+
+        public const string ExpectedHeadingInLoggedUserPartial =
+            "<h2 class=\"page-section-heading text-center text-uppercase text-secondary mb-5 mt-5\">Coming up events</h2>";
+
+        public const string ExpectedHeadingInFaqView = 
+            "<h1 class=\"page-section-heading text-center text-uppercase text-secondary mb-5 mt-5\">Frequently asked questions</h1>";
+
+        public const string ExpectedHeadingInErrorView = 
+            "<h3 class=\"page-section-heading text-center text-uppercase text-secondary mb-5 mt-5\">Please try again later :)</h3>";
+
+        public const string ExpectedHeadingInContactPage = 
+            "<h2 class=\"page-section-heading text-center text-uppercase text-secondary mb-2 mt-5\">Contact Us</h2>";
+
+        public const string ExpectedMessageNotificationAfterContact = "Your message was send.";
+
+        public const string ExpectedErrorForFullname = "The Fullname field is required.";
+
+        public const string ExpectedErrorForMessage = "Message must be between 20 and 2000 characters long.";
+
+        public const string ExpectedErrorForPhoneFormat = "Incorrect phone number format.";
+
+        public const string ExpectedErrorMessageInvalidLogin = "Invalid login attempt.";
+
+        public const string ExpectedHeaderWhenLogged = "Coming up events";
+    }
+}
