d21f6f157106560a9b6e36ea11537248b2ec064b
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-04-06 22:57:00 +0300

    event part of integration tests.. still trying to figure out the problem with authorized post reques to endpoint... problem is somewhere around antiforgery token or extractor

---
+++ Trails.Test.Web/EventController/EventControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
+using Trails.Test.Web.Helpers;
+using Xunit;
+using static Trails.Test.Web.EventController.EventControllerTestConstants;
+
+namespace Trails.Test.Web.EventController
+{
+    public class EventControllerTest
+    {
+        private readonly WebAppFactoryWithAuth<Program> authenticatedServer;
+        private readonly HttpClient client;
+
+        public EventControllerTest()
+        {
+            this.authenticatedServer = new WebAppFactoryWithAuth<Program>();
+            this.client = authenticatedServer.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/event/create")]
+        public async Task CreateShouldReturnCorrectViewOnGetRequest(string url)
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Contains(ExpectedCreateEventHeading, responseContent);
+        }
+
+
+        //Test will not pass even if I'm sending AntiForgery Token along with data, returns 400 Bad Request
+        //If controllers action method is marked with Ignore AntiForgery Token attribute everything works fine
+        //So problem comes from token...
+        //Tested it both with original asp.net tokens and the custom ones set through Token Extractor and and Web App factory
+        //Nothing worked
+
+        //[Theory]
+        //[InlineData("/event/create")]
+        //public async Task CreateShouldReloadViewIfInputIsIncorrect(string url)
+        //{
+        //    var initialResponse = await client.GetAsync(url);
+        //    var antiForgeryValues = await AfTokenExtractor.ExtractAntiForgeryValues(initialResponse);
+        //    var postRequest = new HttpRequestMessage(HttpMethod.Post, url);
+        //    postRequest.Headers.Add("Cookie", new CookieHeaderValue(AfTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
+        //    var modelData = new Dictionary<string, string>
+        //    {
+        //        {AfTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue},
+        //        {"Name", "Test Event"},
+        //        {"Description", ""},
+        //        {"Length", ""},
+        //        {"StartDate", "2022-04-29T13:55"},
+        //        {"EndDate", "2022-05-07T13:55"},
+        //        {"Type", ""},
+        //        {"DifficultyLevel", ""}
+        //    };
+        //    postRequest.Content = new FormUrlEncodedContent(modelData);
+        //    var response = await client.SendAsync(postRequest);
+        //    response.EnsureSuccessStatusCode();
+        //    var responseString = await response.Content.ReadAsStringAsync();
+        //    Assert.Contains(ExpectedDescriptionError, responseString);
+        //}
+    }
+}
---
+++ Trails.Test.Web/EventController/EventControllerTestConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Trails.Test.Web.EventController
+{
+    public static class EventControllerTestConstants
+    {
+        public const string ExpectedCreateEventHeading = "Add Event";
+        public const string ExpectedDescriptionError = "The Description field is required.";
+        public const string ExpectedStartDateError = "Event must be created or updated atleast three days before it's start.";
+        public const string ExpectedDifficultyLevelError = "Please select difficulty level from the list.";
+        public const string ExpectedImageError = "The Image field is required.";
+    }
+}
