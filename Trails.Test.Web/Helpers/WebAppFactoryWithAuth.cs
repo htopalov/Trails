5f9486ba17cc487b0598a42aa7ca4097ccbafbd7
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2022-04-06 22:55:31 +0300

    integration tests helpers

---
+++ Trails.Test.Web/Helpers/WebAppFactoryWithAuth.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Trails.Data;
+
+namespace Trails.Test.Web.Helpers
+{
+    public class WebAppFactoryWithAuth<T> : WebApplicationFactory<Program>
+        where T : Program
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<TrailsDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<TrailsDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                });
+                var serviceProvider = services.BuildServiceProvider();
+                using var scope = serviceProvider.CreateScope();
+                using var appContext = scope.ServiceProvider.GetRequiredService<TrailsDbContext>();
+                appContext.Database.EnsureCreated();
+
+                services.AddAntiforgery(x =>
+                {
+                    x.Cookie.Name = AfTokenExtractor.AntiForgeryCookieName;
+                    x.FormFieldName = AfTokenExtractor.AntiForgeryFieldName;
+                });
+
+                services.AddAuthentication("Test")
+                    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", options => { });
+
+                
+            });
+        }
+    }
+}
